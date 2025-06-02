f438db0f6e2327220b3e39c87df7d94da0e5b2be
Sebastian <16629355+reckseba@users.noreply.github.com>
2025-06-02 21:54:31 +0200

    pacman no confirm

--- arch.sh
+++ arch.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-sudo pacman -Syu --needed gcc python-virtualenv python-pip git ansible less
+# S Sync; y refresh packages; u upgrade
+sudo pacman -Syu --noconfirm --needed gcc python-virtualenv python-pip git ansible less
 mkdir -p ~/repos/github/reckseba
 echo "Now I will clone the ansible repo. This will prompt for GitHub username and password. Use the github token as password."
 git clone https://github.com/reckseba/ansible-desktop.git ~/repos/github/reckseba
