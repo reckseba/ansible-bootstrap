5a31b4b78ef6cfefc7e096fd6db63ce57c0b1034
Sebastian <16629355+reckseba@users.noreply.github.com>
2025-06-02 21:57:34 +0200

    fix git clone

--- arch.sh
+++ arch.sh
@@ -3,7 +3,7 @@
 sudo pacman -Syu --noconfirm --needed gcc python-virtualenv python-pip git ansible less
 mkdir -p ~/repos/github/reckseba
 echo "Now I will clone the ansible repo. This will prompt for GitHub username and password. Use the github token as password."
-git clone https://github.com/reckseba/ansible-desktop.git ~/repos/github/reckseba
+git clone https://github.com/reckseba/ansible-desktop.git ~/repos/github/reckseba/ansible-desktop
 cd ~/repos/github/reckseba/ansible-desktop
 ansible-playbook playbook.yml
 
